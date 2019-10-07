4f50cfabd5f564ef7a6202cb07cea1cae77f5099
stf <7o5rfu92t@ctrlc.hu>
2019-10-07 20:50:16 +0000

    [mod] added asserts to opaque test

--- pwdsphinx/opaque_test.py
+++ pwdsphinx/opaque_test.py
@@ -10,8 +10,10 @@ resp, sk_s = o.opaque_srvSession(pub, rec)
 sk_u = o.opaque_usrSessionEnd(pwd, resp, sec)
 print "sk_s", repr(sk_s)
 print "sk_u", repr(sk_u)
+assert(sk_s == sk_u)
 print "f(sk_s,0)", repr(o.opaque_f(sk_s, '0'))
 print "f(sk_u,0)", repr(o.opaque_f(sk_u, '0'))
+assert(o.opaque_f(sk_s, '0')==o.opaque_f(sk_u, '0'))
 
 print "trying alternative/private registration flow"
 # alternative/private registration flow:
@@ -25,5 +27,7 @@ resp, sk_s = o.opaque_srvSession(pub, rec)
 sk_u = o.opaque_usrSessionEnd(pwd, resp, sec)
 print "sk_s", repr(sk_s)
 print "sk_u", repr(sk_u)
+assert(sk_s == sk_u)
 print "f(sk_s,1)", repr(o.opaque_f(sk_s, '1'))
 print "f(sk_u,1)", repr(o.opaque_f(sk_u, '1'))
+assert(o.opaque_f(sk_s, '1')==o.opaque_f(sk_u, '1'))
