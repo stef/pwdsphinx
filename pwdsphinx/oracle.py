2bbbc449671a95bb2d294f57b788538729e174dc
stf <7o5rfu92t@ctrlc.hu>
2020-01-26 22:57:00 +0000

    [enh] initial boring crypto implementation, with stub for backup and support to store/query encrypted blobs

--- pwdsphinx/oracle.py
+++ pwdsphinx/oracle.py
@@ -1,312 +1,408 @@
 #!/usr/bin/env python3
 
-import asyncio, datetime, os, binascii, shutil, sys
+import socket, sys, ssl, os, datetime, binascii, pysodium, shutil, os.path
 from SecureString import clearmem
-import pysodium
 from pwdsphinx import sphinxlib
 from pwdsphinx.config import getcfg
 cfg = getcfg('sphinx')
 
 verbose = cfg['server'].getboolean('verbose')
 address = cfg['server']['address']
-port = cfg['server']['port']
-datadir = cfg['server']['datadir']
-keydir = cfg['server']['keydir']
+port = int(cfg['server']['port'])
+max_kids = int(cfg['server'].get('max_kids',5))
+datadir = os.path.expanduser(cfg['server']['datadir'])
+ssl_key = cfg['server']['ssl_key']
+ssl_cert = cfg['server']['ssl_cert']
 
 if(verbose):
   cfg.write(sys.stdout)
 
 CREATE=0x00
+READ=0x0f
+BACKUP=0x33
+UNDO=0x55
 GET=0x66
 COMMIT=0x99
 CHANGE=0xaa
+WRITE=0xcc
 DELETE=0xff
 
-def readf(fname):
-  if not os.path.exists(fname):
-    print(fname,'not exist')
-    raise ValueError(b"fail")
-  with open(fname,'rb') as fd:
-    return fd.read()
+def fail(s):
+    if verbose: print('fail')
+    s.send(b'fail') # plaintext :/
+    s.close()
 
-def respond(chal, id, secret = None):
-  path = os.path.expanduser(datadir+binascii.hexlify(id).decode())
-  if not secret:
-    try:
-        secret = readf(path+'/key')
-    except ValueError:
-        return b'fail' # key not found
+def pop(obj, cnt):
+  return obj[:cnt], obj[cnt:]
 
-  if len(secret)!= sphinxlib.DECAF_255_SCALAR_BYTES:
-    if verbose: print("secret wrong size")
-    return b'fail'
+def verify_blob(msg, pk):
+  sig = msg[-64:]
+  msg = msg[:-64]
+  pysodium.crypto_sign_verify_detached(sig, msg, pk)
+  return msg
 
-  try:
-    rule = readf(path+'/rule')
-  except ValueError:
-    return b'fail' # key not found
+def save_blob(path,fname,blob):
+  path = os.path.join(datadir, path, fname)
+  with open(path,'wb') as fd:
+    os.fchmod(fd.fileno(),0o600)
+    fd.write(blob)
 
-  with open(path+'/xpub','rb') as fd:
-    xpk = fd.read()
-  rule = pysodium.crypto_box_seal(rule, xpk)
+# msg format: 0x00|pk[32]|id[32]|alpha[32]|rule[50]|sig[64]
+def create(conn, msg):
+    op,   msg = pop(msg,1)
+    pk,   msg = pop(msg,32)
+    id,   msg = pop(msg,32)
+    alpha,msg = pop(msg,32)
+    rules,msg = pop(msg,50)
+    if msg!=b'':
+      if verbose: print('invalid get msg, trailing content %r' % msg)
+      fail(conn)
+      return
 
-  try:
-    return sphinxlib.respond(chal, secret)+rule
-  except ValueError:
-    if verbose: print("respond fail")
-    return b'fail'
-
-class SphinxOracleProtocol(asyncio.Protocol):
-  def connection_made(self, transport):
-    if verbose:
-      peername = transport.get_extra_info('peername')
-      print('{} Connection from {}'.format(datetime.datetime.now(), peername))
-    self.transport = transport
-
-  def create(self, data):
-    # needs pubkey, id, challenge, rule, sig(id)
-    # returns output from ./response | fail
-    pk = data[171:203]
-    try:
-      data = pysodium.crypto_sign_open(data, pk)
-    except ValueError:
-      print('invalid signature')
-      return b'fail'
-    id = data[1:33]
-    chal = data[33:65]
-    rule = data[65:107]
-    tdir = os.path.expanduser(datadir+binascii.hexlify(id).decode())
-
+    id = binascii.hexlify(id).decode()
+    tdir = os.path.join(datadir,id)
     if os.path.exists(tdir):
-      print(tdir, 'exists')
-      return b'fail' # key already exists
+      if verbose: print("%s exists" % tdir)
+      fail(conn)
+      return
 
+    k=pysodium.randombytes(32)
+
+    try:
+        beta = sphinxlib.respond(alpha, k)
+    except:
+      fail(conn)
+      return
+
+    if not os.path.exists(datadir):
+        os.mkdir(datadir,0o700)
     os.mkdir(tdir,0o700)
 
-    with open(tdir+'/pub','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(pk)
+    save_blob(id,'key',k)
+    save_blob(id,'pub',pk)
+    save_blob(id,'rules',rules)
 
-    xpk = pysodium.crypto_sign_pk_to_box_pk(pk)
-    with open(tdir+'/xpub','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(xpk)
+    conn.send(beta)
 
-    with open(tdir+'/rule','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(rule)
+def load_blob(path,fname,size=None):
+    f = os.path.join(datadir,path,fname)
+    if not os.path.exists(f):
+        if verbose: print('%s does not exist' % f)
+        return
+    with open(f,'rb') as fd:
+        v = fd.read()
+    if size and len(v) != size:
+        if verbose: print("wrong size for %s" % f)
+        return
+    return v
 
-    k=pysodium.randombytes(32)
-    with open(tdir+'/key','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(k)
+# msg format: 0x66|id[32]|alpha[32]
+def get(conn, msg):
+    _, msg = pop(msg,1)
+    id, msg = pop(msg,32)
+    alpha, msg = pop(msg,32)
+    if msg!=b'':
+      if verbose: print('invalid get msg, trailing content %r' % msg)
+      fail(conn)
+      return
 
-    return respond(chal, id)
+    id = binascii.hexlify(id).decode()
+    k = load_blob(id,'key',32)
+    if k is None:
+      # maybe execute protocol with static but random value to not leak which host ids exist?
+      fail(conn)
+      return
 
-  def getpk(self,data):
-    id = data[65:97]
-    tdir = os.path.expanduser(datadir+binascii.hexlify(id).decode())
-    with open(tdir+'/pub','rb') as fd:
-      return fd.read()
+    rules = load_blob(id,'rules', 50)
+    if rules is None:
+        fail(conn)
+        return
 
-  def get(self, data):
-    # needs id, challenge, sig(id)
-    # returns output from ./response | fail
     try:
-      pk = self.getpk(data)
+        beta = sphinxlib.respond(alpha, k)
     except:
-      return b'fail'
+      fail(conn)
+      return
+
+    conn.send(beta+rules)
+
+def auth(msg):
+    id = binascii.hexlify(msg[1:33]).decode()
+    pk = load_blob(id,'pub',32)
+    if pk is None:
+      print('no pubkey found in %s' % id)
+      return
     try:
-      data = pysodium.crypto_sign_open(data, pk)
+      msg = verify_blob(msg,pk)
     except ValueError:
-      print('invalid signature')
-      return b'fail'
-    id = data[1:33]
-    chal = data[33:65]
+      print('invalid signature on msg')
+      return
+    return msg
 
-    return respond(chal, id)
+def read(conn, msg):
+  _, msg = pop(msg,1)
+  id, msg = pop(msg,32)
+  id = binascii.hexlify(id).decode()
 
-  def change(self, data):
-    # needs id, challenge, sig(id)
-    # returns output from ./response | fail
-    try:
-      pk = self.getpk(data)
-    except:
-      return b'fail'
-    try:
-      data = pysodium.crypto_sign_open(data, pk)
-    except ValueError:
-      print('invalid signature')
-      return b'fail'
-    id = data[1:33]
-    chal = data[33:65]
+  blob = load_blob(id,'blob')
+  if blob is None:
+    blob = b''
 
-    tdir = os.path.expanduser(datadir+binascii.hexlify(id).decode())
-    k=pysodium.randombytes(32)
-    with open(tdir+'/new','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(k)
+  conn.send(blob)
 
-    try:
-      rule = readf(tdir+"/rule")
-    except:
-      return b'fail'
+def write(conn, msg):
+  op,msg = pop(msg,1)
+  pk1,msg = pop(msg,32)
+  id,blob = pop(msg,32)
+  id = binascii.hexlify(id).decode()
 
-    try:
-      return respond(chal, id, secret = k)
-    except ValueError:
-      if verbose: print("respond fail")
-      return b'fail'
+  pk0 = load_blob(id,'pub')
+  if pk0 and pk0 != pk1:
+    fail(conn)
+    return
 
-  def commit(self, data):
-    # needs id, sig(id)
-    try:
-      pk = self.getpk(data)
-    except:
-      return b'fail'
-    try:
-      data = pysodium.crypto_sign_open(data, pk)
-    except ValueError:
-      print('invalid signature')
-      return b'fail'
-    id = data[1:33]
-    tdir = os.path.expanduser(datadir+binascii.hexlify(id).decode())
+  if not os.path.exists(datadir):
+    os.mkdir(datadir,0o700)
+  tdir = os.path.join(datadir,id)
+  if not os.path.exists(tdir):
+    os.mkdir(tdir,0o700)
+  save_blob(id,'blob',blob)
+  if pk0 is None:
+    save_blob(id,'pub',pk1)
+  conn.send(b'ok')
 
-    try:
-      with open(tdir+'/new','rb') as fd:
-        k = fd.read()
-    except FileNotFoundError:
-      return b'fail'
+def change(conn, msg):
+  op,   msg = pop(msg,1)
+  id,   msg = pop(msg,32)
+  alpha,msg = pop(msg,32)
+  #print("alpha=",alpha.hex())
+  if msg!=b'':
+    if verbose: print('invalid get msg, trailing content %r' % msg)
+    fail(conn)
+    return
 
-    if(len(k)!=32):
-      return b'fail'
+  id = binascii.hexlify(id).decode()
+  tdir = os.path.join(datadir,id)
+  if not os.path.exists(tdir):
+    if verbose: print("%s doesn't exist" % tdir)
+    fail(conn)
+    return
 
-    with open(tdir+'/key','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(k)
+  k=pysodium.randombytes(32)
+  #print("k=",k.hex())
 
-    os.unlink(tdir+'/new')
-
-    return b'ok'
-
-  def delete(self, data):
-    # needs id, sig(id)
-    # returns ok | fail
-    try:
-      pk = self.getpk(data)
-    except:
-      return b'fail'
-    try:
-      data = pysodium.crypto_sign_open(data, pk)
-    except ValueError:
-      print('invalid signature')
-      return b'fail'
-    id = data[1:33]
-
-    tdir = os.path.expanduser(datadir+binascii.hexlify(id).decode())
-    shutil.rmtree(tdir)
-    return b'ok'
-
-  def data_received(self, data):
-    res = b''
-
-    if verbose:
-      print('Data received: {!r}'.format(data))
-
-    esk,xsk,xpk = getkey(keydir)
-    data = pysodium.crypto_box_seal_open(data,xpk,xsk)
-    clearmem(xsk)
-
-    if data[64] == 0:
-      res = self.create(data)
-    elif data[64] == GET:
-      # needs id, challenge, sig(id)
-      # returns output from ./response | fail
-      res = self.get(data)
-    elif data[64] == CHANGE:
-      # needs id, challenge, sig(id)
-      # changes stored secret
-      # returns output from ./response | fail
-      res = self.change(data)
-    elif data[64] == DELETE:
-      # needs id, sig(id)
-      # returns ok|fail
-      res = self.delete(data)
-    elif data[64] == COMMIT:
-      # needs id, sig(id)
-      # returns ok|fail
-      res = self.commit(data)
-
-    if verbose:
-      print('Send: {!r}'.format(res))
-
-    res=pysodium.crypto_sign(res,esk)
-    clearmem(esk)
-    self.transport.write(res)
-
-    if verbose:
-      print('Close the client socket')
-    self.transport.close()
-
-def getkey(keydir):
-  datadir = os.path.expanduser(keydir)
-  esk = None
   try:
-    with open(datadir+'server-key', 'rb') as fd:
-      esk = fd.read(pysodium.crypto_sign_SECRETKEYBYTES)
-    with open(datadir+'server-xkey', 'rb') as fd:
-      xsk = fd.read(pysodium.crypto_box_SECRETKEYBYTES)
-    with open(datadir+'server-xkey.pub', 'rb') as fd:
-      xpk = fd.read(pysodium.crypto_box_PUBLICKEYBYTES)
-    return esk, xsk, xpk
-  except FileNotFoundError:
-    print("no server key found, generating...")
-    if not os.path.exists(datadir):
-      os.mkdir(datadir,0o700)
-    if esk is None:
-        epk, esk = pysodium.crypto_sign_keypair()
-    else:
-        epk = pysodium.crypto_sign_sk_to_pk(esk)
-    xsk = pysodium.crypto_sign_sk_to_box_sk(esk)
-    xpk = pysodium.crypto_sign_pk_to_box_pk(epk)
-    with open(datadir+'server-key','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(esk)
-    with open(datadir+'server-key.pub','wb') as fd:
-      fd.write(epk)
-    with open(datadir+'server-xkey','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(xsk)
-    with open(datadir+'server-xkey.pub','wb') as fd:
-      fd.write(xpk)
-    print("please share `%s` with all clients"  % (datadir+'server-key.pub'))
-    return esk,xsk,xpk
+      beta = sphinxlib.respond(alpha, k)
+  except:
+    fail(conn)
+    return
+
+  #print("beta=",beta.hex())
+  rules = load_blob(id,'rules', 50)
+  if rules is None:
+      fail(conn)
+      return
+
+  save_blob(id,'new',k)
+  conn.send(beta+rules)
+
+def commit(conn, msg):
+  op,   msg = pop(msg,1)
+  id,   msg = pop(msg,32)
+  alpha,msg = pop(msg,32)
+  #print("alpha=",alpha.hex())
+  if msg!=b'':
+    if verbose: print('invalid get msg, trailing content %r' % msg)
+    fail(conn)
+    return
+
+  id = binascii.hexlify(id).decode()
+  tdir = os.path.join(datadir,id)
+  if not os.path.exists(tdir):
+    if verbose: print("%s doesn't exist" % tdir)
+    fail(conn)
+    return
+
+  k = load_blob(id,'new', 32)
+  if k is None:
+      fail(conn)
+      return
+
+  old = load_blob(id,'key', 32)
+  if old is None:
+      fail(conn)
+      return
+
+  try:
+      beta = sphinxlib.respond(alpha, k)
+  except:
+    fail(conn)
+    return
+
+  #print("beta=",beta.hex())
+  rules = load_blob(id,'rules', 50)
+  if rules is None:
+      fail(conn)
+      return
+
+  save_blob(id,'old',old)
+  save_blob(id,'key',k)
+  os.unlink(os.path.join(tdir,'new'))
+
+  conn.send(beta+rules)
+
+def undo(conn, msg):
+  op,   msg = pop(msg,1)
+  id,   msg = pop(msg,32)
+  alpha,msg = pop(msg,32)
+  #print("alpha=",alpha.hex())
+  if msg!=b'':
+    if verbose: print('invalid get msg, trailing content %r' % msg)
+    fail(conn)
+    return
+
+  id = binascii.hexlify(id).decode()
+  tdir = os.path.join(datadir,id)
+  if not os.path.exists(tdir):
+    if verbose: print("%s doesn't exist" % tdir)
+    fail(conn)
+    return
+
+  k = load_blob(id,'old', 32)
+  if k is None:
+      fail(conn)
+      return
+
+  new = load_blob(id,'key', 32)
+  if new is None:
+      fail(conn)
+      return
+
+  try:
+      beta = sphinxlib.respond(alpha, k)
+  except:
+    fail(conn)
+    return
+
+  #print("beta=",beta.hex())
+  rules = load_blob(id,'rules', 50)
+  if rules is None:
+      fail(conn)
+      return
+
+  save_blob(id,'new',new)
+  save_blob(id,'key',k)
+  os.unlink(os.path.join(tdir,'old'))
+
+  conn.send(beta+rules)
+
+def delete(conn, msg):
+  op,   msg = pop(msg,1)
+  id,   msg = pop(msg,32)
+  if msg!=b'':
+    if verbose: print('invalid get msg, trailing content %r' % msg)
+    fail(conn)
+    return
+
+  id = binascii.hexlify(id).decode()
+  tdir = os.path.join(datadir,id)
+  if not os.path.exists(tdir):
+    if verbose: print("%s doesn't exist" % tdir)
+    fail(conn)
+    return
+  shutil.rmtree(tdir)
+  conn.send(b'ok')
+
+def handler(conn):
+   data = conn.recv(4096)
+   if verbose:
+     print('Data received: {!r}'.format(data))
+
+   # check auth
+   if data[0] in (READ,CHANGE,COMMIT,UNDO,DELETE,BACKUP):
+     data = auth(data)
+     if data is None:
+        fail(conn)
+        conn.close()
+        os._exit(0)
+   elif data[0] in (CREATE,WRITE):
+     pk = data[1:33]
+     try:
+       data = verify_blob(data,pk)
+     except ValueError:
+       fail(conn)
+       conn.close()
+       os._exit(0)
+
+   if data[0] == CREATE:
+     create(conn, data)
+   elif data[0] == GET:
+     get(conn, data)
+   elif data[0] == CHANGE:
+     change(conn, data)
+   elif data[0] == DELETE:
+     delete(conn, data)
+   elif data[0] == COMMIT:
+     commit(conn, data)
+   elif data[0] == UNDO:
+     undo(conn, data)
+   elif data[0] == BACKUP:
+     backup(conn, data)
+   elif data[0] == READ:
+     read(conn, data)
+   elif data[0] == WRITE:
+     write(conn, data)
+   elif verbose:
+     print("unknown op: 0x%02x" % data[0])
+
+   conn.close()
+   os._exit(0)
 
 def main():
-  loop = asyncio.get_event_loop()
-  # Each client connection will create a new protocol instance
-  coro = loop.create_server(SphinxOracleProtocol, address, port)
-  server = loop.run_until_complete(coro)
+    ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ctx.load_cert_chain(certfile=ssl_cert, keyfile=ssl_key)
 
-  esk,xsk,xpk = getkey(keydir)
-  if None in (esk,xsk,xpk):
-    print("no server keys available.\nabort")
-    sys.exit(1)
-  clearmem(xsk)
-  clearmem(esk)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        s.bind((address, port))
+    except socket.error as msg:
+        print('Bind failed. Error Code : %s Message: ' % (str(msg[0]), msg[1]))
+        sys.exit()
+    #Start listening on socket
+    s.listen()
+    kids = []
+    try:
+        # main loop
+        while 1:
+            #wait to accept a connection - blocking call
+            conn, addr = s.accept()
+            if verbose:
+                print('{} Connection from {}:{}'.format(datetime.datetime.now(), addr[0], addr[1]))
+            conn = ctx.wrap_socket(conn, server_side=True)
 
-  # Serve requests until Ctrl+C is pressed
-  if verbose:
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
-  try:
-    loop.run_forever()
-  except KeyboardInterrupt:
-    pass
+            while(len(kids)>max_kids):
+                pid, status = os.waitpid(0,0)
+                kids.remove(pid)
 
-  # Close the server
-  server.close()
-  loop.run_until_complete(server.wait_closed())
-  loop.close()
+            pid=os.fork()
+            if pid==0:
+              ssl.RAND_add(os.urandom(16),0.0)
+              try:
+                handler(conn)
+              finally:
+                conn.shutdown(socket.SHUT_RDWR)
+                conn.close()
+            else:
+                kids.append(pid)
+
+            pid, status = os.waitpid(0,os.WNOHANG)
+            if(pid,status)!=(0,0):
+                kids.remove(pid)
+
+    except KeyboardInterrupt:
+        pass
+    s.close()
 
 if __name__ == '__main__':
   main()
