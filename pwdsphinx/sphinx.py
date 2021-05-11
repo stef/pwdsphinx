44ada28a79173ab76ccf36c0512570032b8d506b
stf <7o5rfu92t@ctrlc.hu>
2021-05-11 20:39:52 +0000

    [doc] updated comment

--- pwdsphinx/sphinx.py
+++ pwdsphinx/sphinx.py
@@ -30,7 +30,7 @@ address = socket.gethostbyname(hostname)
 port = int(cfg['client'].get('port',2355))
 datadir = os.path.expanduser(cfg['client'].get('datadir','~/.config/sphinx'))
 ssl_cert = os.path.expanduser(cfg['client'].get('ssl_cert')) # only for dev, production system should use proper certs!
-#  make RWD optional in (sign|seal)key, if it is b'' then this protects against
+#  make RWD optional in signkey, if it is b'' then this protects against
 #  offline master pwd bruteforce attacks, drawback that for known (host,username) tuples
 #  the seeds/blobs can be controlled by an attacker if the masterkey is known
 rwd_keys = not not cfg['client'].get('rwd_keys',False)
@@ -328,7 +328,7 @@ def create(s, pwd, user, host, char_classes='uld', symbols=bin2pass.symbols, siz
 
   checkdigit = pysodium.crypto_generichash(CHECK_CTX, rwd, 1)[0]
 
-  rule = pack_rule(char_classes, symbols, size, checkdigit) # todo fix checkdigit
+  rule = pack_rule(char_classes, symbols, size, checkdigit)
   # send over new signed(pubkey, rule)
   msg = b''.join([pk, rule])
   msg = sign_blob(msg, id, rwd)
