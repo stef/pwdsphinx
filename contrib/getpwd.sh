085b042e03c6799a91842f5d34e09bb45d98787b
stf <7o5rfu92t@ctrlc.hu>
2022-02-12 21:09:42 +0000

    [enh] getpwd.sh now explicitly shows which user/host the password is queried for

--- contrib/getpwd.sh
+++ contrib/getpwd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-echo -en 'SETTITLE sphinx password prompt\nSETPROMPT sphinx password\nGETPIN\n' | pinentry | grep '^D' | cut -c3- | tr -d '\n' 
+prompt=${1:-sphinx}
+echo -en "SETTITLE sphinx password prompt\nSETPROMPT ${prompt} password\nGETPIN\n" | pinentry | grep '^D' | cut -c3- | tr -d '\n' 
--- contrib/type-pwd.sh
+++ contrib/type-pwd.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
 
-getpwd.sh | sphinx get "$1" "$2" | exec-on-click.sh xdotool type --clearmodifiers '$(cat)'
+getpwd.sh "$1@$2" | sphinx get "$1" "$2" | exec-on-click.sh xdotool type --clearmodifiers '$(cat)'
