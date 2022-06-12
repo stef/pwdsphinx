3d24bae18728b3d0084e6e9d12e575da34f827e7
stf <7o5rfu92t@ctrlc.hu>
2022-06-12 12:27:02 +0000

    [mod] no need to configure mouse device for exec-on-click

--- contrib/exec-on-click.sh
+++ contrib/exec-on-click.sh
@@ -2,21 +2,7 @@
 
 # depends on xinput
 
-# get the name of your mouse by running
-# xinput --list --short
-[[ -f /etc/sphinx/mousedev ]] && . /etc/sphinx/mousedev 2>/dev/null
-[[ -f ~/.config/sphinx/mousedev ]] && . ~/.config/sphinx/mousedev 2>/dev/null
-[[ -f ${0%%/*}/mousedev ]]&& . ${0%%/*}/mousedev 2>/dev/null
-
-[[ -z "$MOUSEDEV" ]] && {
-    echo "unconfigured mouse device in ${0##*/}, please run 'xinput --list --short | fgrep pointer'"
-    echo "select the name of your mouse device, and then set it by running:"
-    echo "echo MOUSEDEV='the name of your mouse device' >$config_file"
-    echo "where $config_file is one of /etc/sphinx/mousedev, ~/.config/sphinx/mousedev, or ${0%%/*}/mousedev"
-    exit 1
-}
-
-MOUSEID=$(xinput --list --short | fgrep "$MOUSEDEV" | sed 's/.*id=\([0-9]*\).*/\1/')
+MOUSEID=$(xinput --list --short | fgrep "Virtual core pointer" | sed 's/.*id=\([0-9]*\).*/\1/')
 THIS=$$
 # wait until left mouse click
 exec 2>/dev/null
