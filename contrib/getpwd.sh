0b8c40deded7ac3700bcbb6f5c2fff3a64002c51
stf <7o5rfu92t@ctrlc.hu>
2022-10-12 20:56:59 +0200

    [mod] moved /debian into seperate branch

--- contrib/getpwd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-prompt=${1:-sphinx}
-echo -en "SETTITLE sphinx password prompt\nSETPROMPT ${prompt} password\nGETPIN\n" | pinentry | grep '^D' | cut -c3- | tr -d '\n' 
--- contrib/type-pwd.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-getpwd.sh "$1@$2" | sphinx get "$1" "$2" | exec-on-click.sh xdotool type --clearmodifiers '$(cat)'
