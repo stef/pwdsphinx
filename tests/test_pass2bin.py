e28b5af1f0a1846468fdacc882068b20c98e294c
stf <7o5rfu92t@ctrlc.hu>
2022-12-26 15:33:28 +0000

    [fix] pass2bin unittests

--- tests/test_pass2bin.py
+++ tests/test_pass2bin.py
@@ -12,8 +12,8 @@ from pwdsphinx import bin2pass
 class TestRules(unittest.TestCase):
     def test_invert_simple(self):
       target = "this is a pass2bin test string"
-      rwd = bin2pass.pass2bin(target)
-      self.assertEqual(bin2pass.derive(rwd, 'uld', len(target), bin2pass.symbols), target)
+      rwd, classes, symbols = bin2pass.pass2bin(target)
+      self.assertEqual(bin2pass.derive(rwd, classes, len(target), symbols), target)
 
     def test_invert_too_long(self):
       target = "this is a pass2bin test stringthis is a pass2bin test stringthis is a pass2bin test stringthis is a pass2bin test stringthis is a pass2bin test string"
@@ -24,42 +24,43 @@ class TestRules(unittest.TestCase):
       for i in range(1,len(chars)):
         target=''.join(chars[:i])
         try:
-          rwd = bin2pass.pass2bin(target)
+          rwd, classes, symbols = bin2pass.pass2bin(target)
         except OverflowError:
           break
-        self.assertEqual(bin2pass.derive(rwd, 'uld', len(target), bin2pass.symbols), target)
+        self.assertEqual(bin2pass.derive(rwd, classes, len(target), symbols), target)
 
     def test_invert_reviter(self):
       chars = bin2pass.allchars
       for i in range(1,len(chars)):
         target=''.join(chars[-i:])
         try:
-          rwd = bin2pass.pass2bin(target)
+          rwd,classes, symbols = bin2pass.pass2bin(target)
         except OverflowError:
           break
-        self.assertEqual(bin2pass.derive(rwd, 'uld', len(target), bin2pass.symbols), target)
+        self.assertEqual(bin2pass.derive(rwd, classes, len(target), symbols), target)
 
     def test_invert_random(self):
       chars = bin2pass.allchars
       for _ in range(1000):
          target=''.join(random.choices(chars,k=random.randrange(1,39)))
-         rwd = bin2pass.pass2bin(target)
-         self.assertEqual(bin2pass.derive(rwd, 'uld', len(target), bin2pass.symbols), target)
+         rwd,classes, symbols = bin2pass.pass2bin(target)
+         self.assertEqual(bin2pass.derive(rwd, classes, len(target), symbols), target)
 
     def test_all_zeroes(self):
         logbase = int(math.log(1<<256, len(bin2pass.allchars)))
         target = bin2pass.allchars[0] * (logbase-1) + bin2pass.allchars[1]
         for _ in range(logbase):
-          rwd = bin2pass.pass2bin(target)
-          self.assertEqual(bin2pass.derive(rwd, 'uld', len(target), bin2pass.symbols), target)
+          rwd,classes, symbols = bin2pass.pass2bin(target)
+          self.assertEqual(bin2pass.derive(rwd, classes, len(target), symbols), target)
           target = target[1:]+bin2pass.allchars[0]
 
     def test_short_zeroes(self):
         logbase = int(math.log(1<<256, len(bin2pass.allchars)))
         target = bin2pass.allchars[0] * (logbase//2) + bin2pass.allchars[1]
         for _ in range(len(target)):
-          rwd = bin2pass.pass2bin(target)
-          self.assertEqual(bin2pass.derive(rwd, 'uld', len(target), bin2pass.symbols), target)
+          ctarget = ''.join(target)
+          (rwd,classes,symbols) = bin2pass.pass2bin(ctarget)
+          self.assertEqual(bin2pass.derive(rwd, classes, len(ctarget), symbols), ctarget)
           target = target[1:]+bin2pass.allchars[0]
 
 if __name__ == '__main__':
