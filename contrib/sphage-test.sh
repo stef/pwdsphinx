264c5679f96d0c3a5dccc72387df2a51a4f7d69a
stf <7o5rfu92t@ctrlc.hu>
2023-06-26 23:27:06 +0200

    [mod] renamed sphage.py to sphage

--- contrib/sphage-test.sh
+++ contrib/sphage-test.sh
@@ -7,10 +7,10 @@ rwd=$(echo "asdf" | sha256sum | cut -d' ' -f 1 | rax2 -s)
 
 # convert rwd to age "identity" (privkey)
 privkey=$(mktemp)
-echo "$rwd" | python3 sphage.py privkey >"$privkey"
+echo "$rwd" | python3 sphage privkey >"$privkey"
 
 # convert rwd to age "recipient" (pubkey)
-pubkey=$(echo -n "$rwd" | python3 sphage.py pubkey)
+pubkey=$(echo -n "$rwd" | python3 sphage pubkey)
 
 # encrypt and decrypt hello world using the above key pair derived from rwd
 echo "hello world" | age -r $pubkey | age --decrypt -i "$privkey"
