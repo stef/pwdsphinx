a9849dad0224b35b1ed6f6355bae64baefadad10
stf <7o5rfu92t@ctrlc.hu>
2023-07-25 22:05:59 +0200

    [mod] bumped to v1.0.15

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class BuildMakefilesBuild(SetuptoolsBuild):
         SetuptoolsBuild.run(self)
 
 setup(name = 'pwdsphinx',
-       version = '1.0.14',
+       version = '1.0.15',
        description = 'SPHINX password protocol',
        license = "GPLv3",
        author = 'Stefan Marsiske',
