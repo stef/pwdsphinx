433a04b4ec077da5aaf4864831406ea8a0ff8005
stf <7o5rfu92t@ctrlc.hu>
2024-12-02 16:29:36 +0100

    [fix] typo

--- pwdsphinx/sphinx.py
+++ pwdsphinx/sphinx.py
@@ -493,13 +493,13 @@ def create(m, pwd, user, host, char_classes='uld', symbols=bin2pass.symbols, siz
   else:
       checkdigit = 0
 
-  if target and not user.startswith('raw;//'):
+  if target and not user.startswith('raw://'):
     trwd, char_classes, symbols = bin2pass.pass2bin(target, None)
     xormask = xor(pysodium.crypto_generichash(PASS_CTX, rwd),trwd)
     size = len(target)
     #char_classes = 'uld'
     #symbols = bin2pass.symbols
-  elif user.startswith('raw://'):
+  elif target and user.startswith('raw://'):
     xormask = xor(pysodium.crypto_generichash(PASS_CTX, rwd),target)
   else:
     _uppers = set([x.decode('utf8') for x in bin2pass.sets['u']])
