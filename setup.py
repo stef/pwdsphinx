1d045f81f7bd78b0ec7f1f2e6ca9d817588fe8da
stf <7o5rfu92t@ctrlc.hu>
2025-03-21 02:51:14 +0100

    [mod] bumped to v2.0.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class BuildMakefilesBuild(SetuptoolsBuild):
         SetuptoolsBuild.run(self)
 
 setup(name = 'pwdsphinx',
-       version = '2.0.2',
+       version = '2.0.3',
        description = 'SPHINX password protocol',
        license = "GPLv3",
        author = 'Stefan Marsiske',
